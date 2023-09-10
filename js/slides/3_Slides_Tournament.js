50684457296ab32298c3b85169b55520d0df9cc3
Arefeh Ahmadi <arefehahmadi902@gmail.com>
2023-09-10 16:03:19 +0330

    clean up tournoments slides

--- js/slides/3_Slides_Tournament.js
+++ js/slides/3_Slides_Tournament.js
@@ -231,4 +231,23 @@ SLIDES.push({
 	onend: function(self){
 		self.clear();
 	}
+}, {
+
+	onstart: function(self){
+
+		var o = self.objects;
+
+		
+		self.add({
+			id:"btmWords", type:"TextBox", text_id:"oneoff_last_questionـv2",
+			x:130, y:300, width:700, height:100, align:"center"
+		});
+
+		_hide(o.btmWords), _fadeIn(o.btmWords, 150+10);
+
+	},
+
+	onend: function(self){
+		self.clear();
+	}
 });
